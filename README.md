92cd2b9f4d8866319fdcf2cda4465d27c1872462
acw573 <acw573@stern.nyu.edu>
2021-06-14 20:34:30 -0400

    Initial repo setup

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # robo-advisor-project
+
+## Prerequisites
+
++ Anaconda
++ Python
++ Pip
+
+##  Installation
+
+Clone the repo onto your computer and navigate there from the command line:
+
+```sh
+cd robo-advisor-screencast
+```
+
+
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
 
 load_dotenv() # go get env vars from the .env file
 
@@ -11,9 +12,31 @@ ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 # make a request
 
-symbol = "MSFT" # ask for a user input
+symbol = input("Please input a ticker symbol: ") # ask for a user input
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
 
 response = requests.get(request_url)
-print(type(response))
-print(response.text)
+# print(type(response))
+# print(response.text)
+
+parsed_response = json.loads(response.text)
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+breakpoint()
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: {last_refreshed}")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
